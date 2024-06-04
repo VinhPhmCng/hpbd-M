75322886489a7a0747408c3edeab06bcec9bce8a
VPC <111203113+VinhPhmCng@users.noreply.github.com>
2024-06-04 15:42:54 +0700

    Update contents
    Test music

--- script/main.js
+++ script/main.js
@@ -17,12 +17,32 @@ const fetchData = () => {
 
         // Check if the iteration is over
         // Run amimation if so
-        if ( dataArr.length === dataArr.indexOf(customData) + 1 ) {
-          animationTimeline();
-        } 
+        //if ( dataArr.length === dataArr.indexOf(customData) + 1 ) {
+          //animationTimeline();
+        //} 
       });
     });
 };
+
+window.addEventListener('load', () => {
+  Swal.fire({
+    title: 'Do you want to play music in the background?',
+    // text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Yes',
+    cancelButtonText: 'No',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      document.querySelector('.song').play();
+      animationTimeline();
+    } else {
+      animationTimeline();
+    }
+  });
+});
 
 // Animation Timeline
 const animationTimeline = () => {
